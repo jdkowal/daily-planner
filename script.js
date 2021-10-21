c3af6ca265e67ce2f893b75baa3de9a9c0e7f58b
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-10-21 09:53:10 -0500

    colors change

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ $(document).ready(function () {
     })
     
     function changeBackground() {
-        var currentTime = moment().hours();
+        var currentTime = new Date().getHours();
         
         $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("-")[1]);
+            var blockTime = parseInt($(this).attr("id").split("-")[0]);
             
             if (blockTime < currentTime) {
-                // $(this).removeClass("future");
-                // $(this).removeClass("present");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
                 $(this).addClass("past");
             }
             else if (blockTime === currentTime) {
                 $(this).removeClass("past");
-                // $(this).removeClass("future");
+                $(this).removeClass("future");
                 $(this).addClass("present");
             }
             else {
@@ -37,18 +37,18 @@ $(document).ready(function () {
         });
     }
     changeBackground();
-    var interval = setInterval(changeBackground, 15000);
+    // var interval = setInterval(changeBackground, 15000);
     
     $("#8-hour .task").val(localStorage.getItem("8-hour"));
     $("#9-hour .task").val(localStorage.getItem("9-hour"));
     $("#10-hour .task").val(localStorage.getItem("10-hour"));
     $("#11-hour .task").val(localStorage.getItem("11-hour"));
     $("#12-hour .task").val(localStorage.getItem("12-hour"));
-    $("#1-hour .task").val(localStorage.getItem("1-hour"));
-    $("#2-hour .task").val(localStorage.getItem("2-hour"));
-    $("#3-hour .task").val(localStorage.getItem("3-hour"));
-    $("#6-hour .task").val(localStorage.getItem("6-hour"));
-    $("#7-hour .task").val(localStorage.getItem("7-hour"));
+    $("#13-hour .task").val(localStorage.getItem("13-hour"));
+    $("#14-hour .task").val(localStorage.getItem("14-hour"));
+    $("#15-hour .task").val(localStorage.getItem("15-hour"));
+    $("#16-hour .task").val(localStorage.getItem("16-hour"));
+    $("#17-hour .task").val(localStorage.getItem("17-hour"));
     
     $("#currentDay").text(moment().format("MMM Do, YYYY h:mm"));
 });
